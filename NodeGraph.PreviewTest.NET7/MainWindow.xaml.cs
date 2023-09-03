d9a9445262d02057697e647650f7e80f9a327a96
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-09-03 13:02:21 +0530

    Added new net 7 test project

---
+++ NodeGraph.PreviewTest.NET7/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace NodeGraph.PreviewTest.NET7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
